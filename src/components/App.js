c9181c2e687a79bb55a78dee63df882356a8dba6
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-12-06 15:20:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,6 @@ import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
-const App = () => {
-  return (
-    <Welcome name="" />
-  )
-}
-
+const App = () => {  return (    <Welcome name="" />  )}
 
 export default App;
